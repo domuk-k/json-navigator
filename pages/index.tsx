61907208db6954aff977bb720d310370cd24b62b
Ollie, DongWook Kim <dannyworks102@gmail.com>
2022-02-20 21:30:24 +0900

    fix: 기본 파일 업로드 기능 구현
    - json파일 업로드와 json을 js객체로 파싱한 객체를 상태로 관리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import UploadForm from '../components/UploadForm';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <main>
-      <h1 className="text-xl">JSON navigator</h1>
+      <h1 className="text-2xl mb-8">JSON navigator</h1>
+      <UploadForm />
     </main>
   );
 };
