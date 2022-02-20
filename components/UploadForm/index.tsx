21947e16251679e01847704b1407c3149cb12653
Ollie, DongWook Kim <dannyworks102@gmail.com>
2022-02-21 00:24:02 +0900

    fix: 파일업로드 관련 로직 제거 - 커스텀훅 활용

--- components/UploadForm/index.tsx
+++ components/UploadForm/index.tsx
@@ -1,39 +1,32 @@
-import React, { useCallback, useState, useRef, type FormEvent } from 'react';
+import useReadFileAsJSON from 'hooks/useReadFileAsJSON';
+import React, { useCallback, useState } from 'react';
 
 const UploadForm = () => {
-  const [json, setJson] = useState<object>();
+  const [json, setJson] = useState<object | null>();
 
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File>();
 
-  const handleSubmit = useCallback((event: FormEvent) => {
-    event.preventDefault();
+  useReadFileAsJSON(file, {
+    onLoad: setJson,
+  });
 
-    if (!fileInputRef) return;
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files?.[0];
 
-    const fileReader = new FileReader();
+      if (!file) {
+        return;
+      }
 
-    fileReader.onload = (event) => {
-      const fileAsText = event.target?.result as string;
-
-      const parsedJSONData = JSON.parse(fileAsText);
-
-      setJson(parsedJSONData);
-    };
-
-    if (!fileInputRef.current?.files?.[0]) {
-      return;
-    }
-
-    fileReader.readAsText(fileInputRef.current.files[0]);
-  }, []);
+      setFile(file);
+    },
+    []
+  );
 
   return (
-    <form className="flex flex-col" id="upload" onSubmit={handleSubmit}>
+    <form className="flex flex-col" id="upload">
       <label htmlFor="file">업로드할 파일</label>
-      <input type="file" accept=".json" ref={fileInputRef} />
-      <button className="w-20 px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm">
-        업로드
-      </button>
+      <input type="file" accept=".json" onChange={handleChange} />
     </form>
   );
 };
