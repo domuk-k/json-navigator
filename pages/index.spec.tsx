465c6126fcdbb5947f283d8ee5149b1f621a8bd5
Ollie, DongWook Kim <dannyworks102@gmail.com>
2022-02-21 01:52:56 +0900

    test: 실패하는 test해결 - Provider 도입

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,10 +1,15 @@
+import { LoadedJSONProvider } from '@/context/loadedJSON';
 import { render, screen } from '@testing-library/react';
 
 import Home from '.';
 
 describe('index test', () => {
   it('should render page title', () => {
-    render(<Home />);
+    render(
+      <LoadedJSONProvider>
+        <Home />
+      </LoadedJSONProvider>
+    );
     expect(
       screen.getByRole('heading', { name: 'JSON navigator' })
     ).toBeVisible();
