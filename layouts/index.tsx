afcc7a8fedf90903a1e3a738de5dba5aedc0b067
Ollie, DongWook Kim <dannyworks102@gmail.com>
2022-02-23 02:12:05 +0900

    fix: 스타일 수정 - page overflow

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -4,7 +4,7 @@ interface MainLayout {
 
 const MainLayout = ({ children }: MainLayout) => {
   return (
-    <main className="bg-slate-900 h-screen ">
+    <main className="bg-slate-900 h-screen overflow-auto">
       <div className="grid gap-4 p-24">{children}</div>
     </main>
   );
