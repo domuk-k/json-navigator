5a51396a905a29896cf448a378544585cb6d82d3
Ollie, DongWook Kim <dannyworks102@gmail.com>
2022-02-22 14:25:35 +0900

    chore: 배포 에러 수정- page 디렉토리에 test파일 사용할 수 있도록 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ['tsx', 'ts', 'spec.tsx'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import { LoadedJSONProvider } from 'context/loadedJSON';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
